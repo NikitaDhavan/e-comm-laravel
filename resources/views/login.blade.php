f36c7bc9020184138c558d8c9e76c4bec030c0df
dhavan-nikita <nikita.dhavan@learningmate.com>
2021-04-28 19:41:17 +0530

    login part done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,14 +4,15 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4">
-            <form action="">
+            <form action="login" method="post">
+                @csrf
                 <div class="form-group">
                 <label for="exampleInputEmail1">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
                 </div>
                 <div class="form-group">
                 <label for="exampleInputPassword1">Email Address</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter password">
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Enter password">
                 </div>
                 <button type="submit" class="btn btn-default">submit</button>
             </form>
